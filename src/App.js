ac949a40e3468de22fa662e20b92ddf87537e9f6
Wooksang Ryu <today8934@gmail.com>
2024-09-22 11:13:11 +0900

    feature useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-    <div>
-        <h1 className={styles.title}>Welcome back!</h1>
-        <Button text="Click me!" />
-    </div>
-  );
+    const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setValue(counter + 1);
+    const onChange = (event) => {setKeyword(event.target.value)};
+
+    useEffect(() => {
+        console.log("I Run only Once");
+    }, []);
+    useEffect(() => {
+        console.log("I run when 'keyword' changes");
+    }, [keyword]);
+    useEffect(() => {
+        console.log("I run when 'counter' changes");
+    }, [counter]);
+
+    return (
+        <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search Here" />
+            <h1>{counter}</h1>
+            <button onClick={onClick}>click me</button>
+        </div>
+    );
 }
 
 export default App;
