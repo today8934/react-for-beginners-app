1322f1e8511f3547599b5680020fa9746cb3240f
Wooksang Ryu <today8934@gmail.com>
2024-09-22 11:42:23 +0900

    feature: cleanup function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import {useState, useEffect} from "react";
 
+function Hello() {
+    function hiFunction() {
+        console.log("hi");
+        return byeFunction;
+    }
+
+    function byeFunction() {
+        console.log("bye");
+    }
+
+    useEffect(hiFunction, []);
+    return <h1>Hello</h1>;
+}
+
 function App() {
     const [counter, setValue] = useState(0);
     const [keyword, setKeyword] = useState("");
     const onClick = () => setValue(counter + 1);
     const onChange = (event) => {setKeyword(event.target.value)};
+
+    const [showing, setShowing] = useState(false);
+    const onShow = () => setShowing((prev) => !prev);
 
     useEffect(() => {
         console.log("I Run only Once");
@@ -18,6 +35,8 @@ function App() {
 
     return (
         <div>
+            {showing ? <Hello /> : null}
+            <button onClick={onShow}>{showing ? "Hide" : "Show"}</button>
             <input value={keyword} onChange={onChange} type="text" placeholder="Search Here" />
             <h1>{counter}</h1>
             <button onClick={onClick}>click me</button>
